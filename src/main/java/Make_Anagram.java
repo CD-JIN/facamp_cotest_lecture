e3627dd0fb4b5b288155fb36dd7a263f961426ba
CD-JIN <92803648+CD-JIN@users.noreply.github.com>
2023-03-20 21:51:46 +0900

    클린코딩

--- src/main/java/Make_Anagram.java
+++ src/main/java/Make_Anagram.java
@@ -2,24 +2,25 @@
 import java.util.Scanner;
 
 public class Make_Anagram {
+
+    public static int[] get_count(String str) {
+        int[] count = new int[26];
+        for(int i = 0; i < str.length(); i++) {
+            count[str.charAt(i)- 'a']++;
+        }
+        return count;
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String str1 = sc.next();
         String str2 = sc.next();
         int cnt = 0;
 
-        int[] arr1 = new int[26];
-        int[] arr2 = new int[26];
+        int[] arr1 = get_count(str1);
+        int[] arr2 = get_count(str2);
 
-        for(int i = 0; i < str1.length(); i++) {
-            arr1[str1.charAt(i)- 'a']++;
-        }
-
-        for(int i = 0; i < str2.length(); i++) {
-            arr2[str2.charAt(i) - 'a']++;
-        }
         for(int i = 0; i < arr1.length; i++) {
-            if(arr1[i] != arr2[i]) cnt += Math.abs(arr1[i] - arr2[i]);
+            cnt += Math.abs(arr1[i] - arr2[i]);
         }
 
         System.out.println(cnt);
