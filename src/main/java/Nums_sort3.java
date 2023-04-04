ac67d6d75a6eeddc916d3350e9e7efe1833db476
CD-JIN <92803648+CD-JIN@users.noreply.github.com>
2023-04-04 23:10:06 +0900

    수 정렬하기(No-10989)

---
+++ src/main/java/Nums_sort3.java
@@ -0,0 +1,24 @@
+
+import java.io.*;
+
+public class Nums_sort3 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[10001];
+
+        for(int i = 0; i < N; i++) {
+            arr[Integer.parseInt(br.readLine())]++;
+        }
+
+        for(int i = 0; i < arr.length; i++) {
+            while (arr[i]-- > 0) {
+                bw.write(i+"\n");
+            }
+        }
+        bw.flush();
+    }
+}
